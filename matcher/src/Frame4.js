7eb6c68e4ada83d2cef98b6e51625d19e2493c3d
Arkin Navani <arkin.navani@gmail.com>
2025-04-09 16:17:07 -0500

    Front End

---
+++ matcher/src/Frame4.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Button } from "./Button";
+import "./style.css";
+
+export const Frame = () => {
+  return (
+    <div className="frame">
+      <div className="div-2">
+        <div className="rectangle" />
+
+        <div className="overlap-group">
+          <p className="time-time-time">
+            Time 1: <br />
+            <br />
+            Time 2: <br />
+            <br />
+            Time 3:
+            <br /> <br />
+            Time 4: <br />
+            <br />
+            Time 5:
+          </p>
+
+          <Button
+            buttonClassName="design-component-instance-node"
+            className="button-instance"
+            spanClassName="button-2"
+            text="Next"
+          />
+        </div>
+
+        <div className="text-wrapper-2">Profile Creation</div>
+
+        <button className="button-wrapper">
+          <button className="button-3">About Us</button>
+        </button>
+      </div>
+    </div>
+  );
+};
---
+++ matcher/src/Frame5.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./style.css";
+
+export const Frame = () => {
+  return (
+    <div className="frame">
+      <div className="div">
+        <div className="rectangle" />
+
+        <div className="overlap-group">
+          <p className="study-style-group">
+            Study Style: <br />
+            <br />
+            Group Size: <br />
+            <br />
+            Study Location 1:
+            <br /> <br />
+            Study Location 2: <br />
+            <br />
+            Study Location 3:
+          </p>
+
+          <button className="button">
+            <button className="text-wrapper">Finish</button>
+          </button>
+        </div>
+
+        <div className="text-wrapper-2">Profile Creation</div>
+
+        <button className="button-wrapper">
+          <button className="button-2">About Us</button>
+        </button>
+      </div>
+    </div>
+  );
+};
